98625a34cbdb7e7c1ee2652f1c2716f691343fa7
Nacer Bouchekhima <nbouchek@gmail.com>
2020-05-09 15:47:55 +0100

    Enhance ProductListComponent to read category id param

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,24 +1,23 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {Product} from '../common/product';
-import {map} from 'rxjs/operators';
-
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Product} from "../common/product";
+import {map} from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductService {
-  private baseUrl = 'http://localhost:8080/api/products?size=100';
+  private baseUrl = "http://localhost:8080/api/products?size=100";
 
-  constructor(private  httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient) {
   }
 
-  getProductList(): Observable<Product[]> {
+  getProductList(theCategoryId: number): Observable<Product[]> {
+    // @TODO: need to build URL based on category id ... will comeback to this later
     return this.httpClient
       .get<GetResponse>(this.baseUrl)
-      .pipe(map(response => response._embedded.products)
-      );
+      .pipe(map((response) => response._embedded.products));
   }
 }
 
