dd994d1fa5996f20530fa3d75aed5d34cbebf65a
Nacer Bouchekhima <nbouchek@gmail.com>
2020-05-10 10:20:55 +0100

    Update Angular Service to call new URL on Spring Boot app.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,16 +8,17 @@ import {map} from "rxjs/operators";
   providedIn: "root",
 })
 export class ProductService {
-  private baseUrl = "http://localhost:8080/api/products?size=100";
+  private baseUrl = "http://localhost:8080/api/products";
 
   constructor(private httpClient: HttpClient) {
   }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-    // @TODO: need to build URL based on category id ... will comeback to this later
+    // @TODO: need to build URL based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
     return this.httpClient
-      .get<GetResponse>(this.baseUrl)
-      .pipe(map((response) => response._embedded.products));
+      .get<GetResponse>(searchUrl)
+      .pipe(map(response => response._embedded.products));
   }
 }
 
