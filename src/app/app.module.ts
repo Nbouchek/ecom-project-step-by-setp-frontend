ca6b2f96c11bd886016ca600020372dc106389f4
Nacer Bouchekhima <nbouchek@gmail.com>
2020-05-17 11:37:10 +0100

    Product Master view 5- Update ProductService to call URL on Spring Boot App 6- Update HTML page for ProductDetailsComponent to display product details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
 
-import {AppComponent} from './app.component';
-import {ProductListComponent} from './components/product-list/product-list.component';
-import {HttpClientModule} from '@angular/common/http';
-import {ProductService} from './services/product.service';
+import {AppComponent} from "./app.component";
+import {ProductListComponent} from "./components/product-list/product-list.component";
+import {HttpClientModule} from "@angular/common/http";
+import {ProductService} from "./services/product.service";
 import {RouterModule, Routes} from "@angular/router";
-import {ProductCategoryMenuComponent} from './components/product-category-menu/product-category-menu.component';
-import {SearchComponent} from './components/search/search.component';
-import {ProductDetailsComponent} from './components/product-details/product-details.component';
+import {ProductCategoryMenuComponent} from "./components/product-category-menu/product-category-menu.component";
+import {SearchComponent} from "./components/search/search.component";
+import {ProductDetailsComponent} from "./components/product-details/product-details.component";
 
 const routes: Routes = [
-  {path: 'products/:id', component: ProductDetailsComponent},
-  {path: 'search/:keyword', component: ProductListComponent},
-  {path: 'category/:id/:name', component: ProductListComponent},
-  {path: 'category', component: ProductListComponent},
-  {path: 'products', component: ProductListComponent},
-  {path: '', redirectTo: '/products', pathMatch: 'full'},
-  {path: '**', redirectTo: '/products', pathMatch: 'full'},
+  {path: "products/:id", component: ProductDetailsComponent},
+  {path: "search/:keyword", component: ProductListComponent},
+  {path: "category/:id/:name", component: ProductListComponent},
+  {path: "category", component: ProductListComponent},
+  {path: "products", component: ProductListComponent},
+  {path: "", redirectTo: "/products", pathMatch: "full"},
+  {path: "**", redirectTo: "/products", pathMatch: "full"},
 ];
 @NgModule({
   declarations: [
@@ -25,15 +25,11 @@ const routes: Routes = [
     ProductListComponent,
     ProductCategoryMenuComponent,
     SearchComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
   ],
-  imports: [
-    RouterModule.forRoot(routes),
-    BrowserModule,
-    HttpClientModule
-  ],
+  imports: [RouterModule.forRoot(routes), BrowserModule, HttpClientModule],
   providers: [ProductService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {
 }
