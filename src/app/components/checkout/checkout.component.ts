0ffef14f5e1b0c302b6bd84d1a36fd30f8a526fa
Nacer Bouchekhima <nbouchek@gmail.com>
2020-06-07 22:20:43 +0100

    Support Shipping Address.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -18,12 +18,37 @@ export class CheckoutComponent implements OnInit {
         lastName: [""],
         email: [""],
       }),
+      shippingAddress: this.formBuilder.group({
+        street: [""],
+        city: [""],
+        state: [""],
+        country: [""],
+        zipCode: [""],
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [""],
+        city: [""],
+        state: [""],
+        country: [""],
+        zipCode: [""],
+      }),
+      creditCardInformation: this.formBuilder.group({
+        cardType: [""],
+        nameOnCard: [""],
+        cardNumber: [""],
+        securityCode: [""],
+        expirationMonth: [""],
+        expirationYear: [""],
+      }),
     });
   }
 
   onSubmit() {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get("customer").value);
-    console.log("The email address is " + this.checkoutFormGroup.get("customer").value.email);
+    console.log(
+      "The email address is " +
+        this.checkoutFormGroup.get("customer").value.email
+    );
   }
 }
