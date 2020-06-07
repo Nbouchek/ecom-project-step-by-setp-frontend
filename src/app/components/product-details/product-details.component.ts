3782fa0e4afdf8f3e10e743fa8168cca0cbd7373
Nacer Bouchekhima <nbouchek@gmail.com>
2020-06-07 15:40:56 +0100

    Update ProductDEtailsComponent with click handler method.

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from "@angular/core";
-import {Product} from "../../common/product";
-import {ProductService} from "../../services/product.service";
-import {ActivatedRoute} from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { Product } from "../../common/product";
+import { ProductService } from "../../services/product.service";
+import { ActivatedRoute } from "@angular/router";
+import { CartService } from "src/app/services/cart.service";
+import { CartItem } from "src/app/common/cart-item";
 
 @Component({
   selector: "app-product-details",
@@ -13,9 +15,9 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private productService: ProductService,
+    private cartService: CartService,
     private route: ActivatedRoute
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
@@ -29,5 +31,13 @@ export class ProductDetailsComponent implements OnInit {
     this.productService.getProduct(theProductId).subscribe((data) => {
       this.product = data;
     });
+  }
+
+  addToCart() {
+    console.log(
+      `Adding to cart: ${this.product.name}, ${this.product.unitPrice} `
+    );
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 }
